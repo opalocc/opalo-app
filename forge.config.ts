eb56e2e7ff0cae494be1f51b900191bda5796509
Alvaro Lorente <contact@alvarolorente.dev>
2024-10-20 23:10:25 +0200

    v0.2.5

--- forge.config.ts
+++ forge.config.ts
@@ -11,9 +11,6 @@ const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
     icon: 'src/assets/favicon',
-    osxSign: {
-      "identity": process.env.MAC_APPLE_ID,
-    },
     osxNotarize: {
       appleId: process.env.MAC_APPLE_ID,
       appleIdPassword: process.env.MAC_APPLE_ID_PWD,
